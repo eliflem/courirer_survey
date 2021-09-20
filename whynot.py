e3c51447182d1625b6cdc81957ce0749baf597c9
eliflem <70978219+eliflem@users.noreply.github.com>
2021-09-20 15:33:48 +0300

    Update whynot.py

--- whynot.py
+++ whynot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[23]:
+# In[2]:
 
 
 import streamlit as st
@@ -10,106 +10,122 @@ import pandas as pd
 import time
 
 
-# In[3]:
+# In[50]:
 
 
-headers = {'Authorization': st.secrets["token"], 'Accept': 'application/json', 'Content-Type': 'application/json'}
+headers = {'Authorization': st.secrets["token"] , 'Accept': 'application/json', 'Content-Type': 'application/json'}
+headers
 
 
 # In[2]:
 
 
-st.title("Help us to get to know you better")
-st.text("We will use your answers to provide you the most suitable jobs")
+st.title("Sizi Daha İyi Tanımamıza Yardımcı Olur Musunuz")
+st.text("Vereceğiniz bilgileri size daha uygun gönderiler sağlayabilmek için kullanacağız.")
 
+
+# In[ ]:
+
+
+with st.form(key='my_form'):
+    st.text("Lütfen sistemde kayıtlı olan telefon numaranızı giriniz")
+    phone = st.text_input("Telefon numaranız:")
+    st.text("Çalışmak istediğiniz bölgeleri seçiniz")
+    regions = st.multiselect('Çalışmak istediğiniz bölgeleri seçiniz', ['R1', 'R2', 'R3', 'R4'])
+    st.text("Please select how will be your weekly working schedule")
+    schedule = st.radio('Choose your daily working schedule', ('Haftada 20 saatten az çalışabilirim', 'Haftada 20 ila 30 saat arası çalışabilirim', 'Haftada 30 saatten fazla çalışabilirim'))
+    company = st.radio('Kazancınıza karşılık fatura kesebileceğiniz bir şirketiniz var mı?', ('Evet', 'Hayır'))
+    submit_button = st.form_submit_button(label='Submit')
+
+
+# In[21]:
+
+
+phone = phone.replace(" ", "")
+if len(phone) == 11:
+    phone = phone[1:11]
+else:
+    phone = phone
+
+
+# #get phone number remove spaces and 0 from mthe beginning
+# phone = phone.replace(" ", "")
+
+# if len(phone) == 11:
+#     phone = phone[1:11]
+# else:
+#     phone = phone
 
 # In[3]:
 
 
-phone = st.text_input("Phone number:")
-
-if not phone:
-    st.write("please enter your phone")
-else:
-    query = {
-    "query" : {
-    "field" : "phone",
-    "operator" : "=",
-    "value" : phone
-    } } 
-    url = "https://api.intercom.io/contacts/search"
-    result = r.post(url, headers=headers, json=query)
-    result = result.json()
-    courier_id = result["data"][0]["id"]
+query = {
+"query" : {
+"field" : "phone",
+"operator" : "=",
+"value" : phone
+} } 
+url = "https://api.intercom.io/contacts/search"
+result = r.post(url, headers=headers, json=query)
+result = result.json()
+courier_id = result["data"][0]["id"]
 
 
-    # In[ ]:
+# st.text("Please select the regions that you prefer to work in. You can choose more than one region.")
+# regions = st.multiselect('Choose your regions', ['R1', 'R2', 'R3', 'R4'])
+
+# In[ ]:
 
 
-    st.text("Please select the regions that you prefer to work in. You can choose more than one region.")
-    regions = st.multiselect('Choose your regions', ['R1', 'R2', 'R3', 'R4'])
+update_1 = {"type": "contact",
+        "id": courier_id,
+        "custom_attributes" : {
+                           "regions": regions
+        }}
 
 
-    # In[ ]:
+# In[12]:
 
 
-    update_1 = {"type": "contact",
-            "id": courier_id,
-            "custom_attributes" : {
-                               "regions": regions
-            }}
+update = r.put("https://api.intercom.io/contacts/"+courier_id+"", headers=headers, json=update_1)
 
 
-    # In[12]:
+# st.text("Please select how will be your weekly working schedule")
+# 
+#     schedule = st.radio('Choose your daily working schedule', ('I will work whole work', 'I will work half week', 'I will work whenever I find time'))
+
+# In[2]:
 
 
-    update = r.put("https://api.intercom.io/contacts/"+courier_id+"", headers=headers, json=update_1)
+update_2 = {"type": "contact",
+ "id": courier_id,
+  "custom_attributes" : {
+                       "schedule": schedule
+}}
 
 
-    # In[ ]:
+# In[ ]:
 
 
-    st.text("Please select how will be your weekly working schedule")
-
-    schedule = st.radio('Choose your daily working schedule', ('I will work whole work', 'I will work half week', 'I will work whenever I find time'))
+update = r.put("https://api.intercom.io/contacts/"+courier_id+"", headers=headers, json=update_2)
 
 
-    # In[2]:
+# st.text("Please select if you have a company")
+# 
+# has_company = st.selectbox('Do you have a company', ('Yes', 'No'))
+
+# In[22]:
 
 
-    update_2 = {"type": "contact",
-     "id": courier_id,
-      "custom_attributes" : {
-                           "schedule": schedule
-    }}
+update_3 = {"type": "contact",
+ "id": courier_id,
+  "custom_attributes" : {
+                       "has_company": has_company
+}}
 
 
-    # In[ ]:
+# In[ ]:
 
 
-    update = r.put("https://api.intercom.io/contacts/"+courier_id+"", headers=headers, json=update_2)
-
-
-    # In[ ]:
-
-
-    st.text("Please select if you have a company")
-
-    has_company = st.selectbox('Do you have a company', ('Yes', 'No'))
-
-
-    # In[22]:
-
-
-    update_3 = {"type": "contact",
-     "id": courier_id,
-      "custom_attributes" : {
-                           "has_company": has_company
-    }}
-
-
-    # In[ ]:
-
-
-    update = r.put("https://api.intercom.io/contacts/"+courier_id+"", headers=headers, json=update_3)
+update = r.put("https://api.intercom.io/contacts/"+courier_id+"", headers=headers, json=update_3)
 
