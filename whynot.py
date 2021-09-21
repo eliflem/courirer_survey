8d2c0514287a8d0912f8392fa407bb5e732950b1
eliflem <70978219+eliflem@users.noreply.github.com>
2021-09-21 15:06:44 +0300

    Update whynot.py

--- whynot.py
+++ whynot.py
@@ -57,10 +57,10 @@ elif external_id(phone) == "not exist":
     st.subheader("Lütfen sistemde kayıtlı olan telefon numaranızı giriniz.")
 else:
     with st.form(key='my_form'):
-        st.text("Hangi bölgelerde çalışmayı tercih edersiniz?")
-        preferred_regions = st.multiselect('Çalışmak istediğiniz bölgeleri seçiniz', ['R1', 'R2', 'R3', 'R4'])
+        #st.text("Hangi bölgelerde çalışmayı tercih edersiniz?")
+        preferred_regions = st.multiselect('Hangi bölgelerde çalışmayı tercih edersiniz?', ['R1', 'R2', 'R3', 'R4'])
         #st.text("Banabikurye'de çalışmaya haftada ne kadar zaman ayırabilirsiniz?")
-        schedule = st.radio("Banabikurye'de çalışmaya haftada ne kadar zaman ayırabilirsiniz?", ('Haftada 20 saatten az çalışabilirim', 'Haftada 20 ila 30 saat arası çalışabilirim', 'Haftada 30 saatten fazla çalışabilirim'))
+        schedule = st.radio("Banabikurye'de çalışmak için haftada ne kadar zaman ayırabilirsiniz?", ('Haftada 20 saatten az çalışabilirim', 'Haftada 20 ila 30 saat arası çalışabilirim', 'Haftada 30 saatten fazla çalışabilirim'))
         has_company = st.radio('Kazancınıza karşılık fatura kesebileceğiniz bir şirketiniz var mı?', ('Evet', 'Hayır'))
         submit_button = st.form_submit_button(label='Gönder')
 
