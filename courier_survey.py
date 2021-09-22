e06b7d5d0b23c42be59466e1938c51c70fc6ece7
eliflem <70978219+eliflem@users.noreply.github.com>
2021-09-22 14:37:05 +0300

    Update courier_survey.py

--- courier_survey.py
+++ courier_survey.py
@@ -57,9 +57,17 @@ elif external_id(phone) == "not exist":
     st.subheader("Lütfen sistemde kayıtlı olan telefon numaranızı giriniz.")
 else:
     with st.form(key='my_form'):
-        #st.text("Hangi bölgelerde çalışmayı tercih edersiniz?")
-        preferred_regions = st.multiselect('Hangi bölgelerde çalışmayı tercih edersiniz? Lütfen listeden seçiniz, birden fazla seçim yapabilirsiniz.', ['AVP1 (Beşiktaş, Şişli, Kağıthane)', 'AVP2 (Fatih, Zeytinburnu)', 'AVP3 (Alibeyköy, Sarıyer, Sultangazi)', 'AVP4 (Gaziosmanpaşa, Esenler, Bayrampaşa, Bağcılar)', 'AVP5 (Bakırköy, Bahçelievler, Güngören, Küçükçekmece)', 'AVP6 (Başakşehir, Arnavutköy)', 'AVP7 (Avcılar, Beylikdüzü, Esenyurt, Büyükçekmece)', 'AND1 (Kadıköy, Üsküdar, Ümraniye, Ataşehir)', 'AND2 (Beykoz, Çekmeköy, Sancaktepe, Sultanbeyli)', 'AND3 (Maltepe, Kartal, Pendik, Tuzla)' ])
-        #st.text("Banabikurye'de çalışmaya haftada ne kadar zaman ayırabilirsiniz?")
+        st.text("Hangi bölgelerde çalışmayı tercih edersiniz? Birden fazla seçim yapabilirsiniz.")
+        avp1 = st.checkbox('AVP1 (Beşiktaş, Şişli, Kağıthane)')
+        avp2 = st.checkbox('AVP2 (Fatih, Zeytinburnu)')
+        avp3 = st.checkbox('AVP3 (Alibeyköy, Sarıyer, Sultangazi)')
+        avp4 = st.checkbox('AVP4 (Gaziosmanpaşa, Esenler, Bayrampaşa, Bağcılar)')
+        avp5 = st.checkbox('AVP5 (Bakırköy, Bahçelievler, Güngören, Küçükçekmece)')
+        avp6 = st.checkbox('AVP6 (Başakşehir, Arnavutköy)')
+        avp7 = st.checkbox('AVP7 (Avcılar, Beylikdüzü, Esenyurt, Büyükçekmece)')
+        and1 = st.checkbox('AND1 (Kadıköy, Üsküdar, Ümraniye, Ataşehir)')
+        and2 = st.checkbox('AND2 (Beykoz, Çekmeköy, Sancaktepe, Sultanbeyli)')
+        and3 = st.checkbox('AND3 (Maltepe, Kartal, Pendik, Tuzla)')
         schedule = st.radio("Banabikurye'de çalışmak için haftada ne kadar zaman ayırabilirsiniz?", ('Haftada 20 saatten az çalışabilirim', 'Haftada 20 ila 30 saat arası çalışabilirim', 'Haftada 30 saatten fazla çalışabilirim'))
         has_company = st.radio('Kazancınıza karşılık fatura kesebileceğiniz bir şirketiniz var mı?', ('Evet', 'Hayır'))
         submit_button = st.form_submit_button(label='Gönder')
@@ -69,11 +77,29 @@ else:
         st.subheader("Vermiş olduğunuz cevaplar için teşekkür ederiz.")
         
     courier_id = external_id(phone)
+    
+    
+    preferred_regions = {}
+    preferred_regions["avp1"] = avp1
+    preferred_regions["avp2"] = avp2
+    preferred_regions["avp3"] = avp3
+    preferred_regions["avp4"] = avp4
+    preferred_regions["avp5"] = avp5
+    preferred_regions["avp6"] = avp6
+    preferred_regions["avp7"] = avp7
+    preferred_regions["and1"] = and1
+    preferred_regions["and2"] = and2
+    preferred_regions["and3"] = and3
+
+    regions = []
+    for key, value in preferred_regions.items():
+             if value == True:
+                regions.append(key)
         
     update_1 = {"type": "contact",
             "id": courier_id,
             "custom_attributes" : {
-                               "preferred_regions": ' '.join(x[0:5] for x in preferred_regions)
+                               "preferred_regions": ' '.join(x for x in regions)
             }}
 
 
